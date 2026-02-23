fa731befc4072d8207d3a817fbe1299efa50beb3
Xander <thiakil@users.noreply.github.com>
2026-02-23 13:21:41 +0800

    Java dir fixes (#20)
    * use folder found during system jre check to run the command
    
    * find java exe to test install, run the first one found if multiple

--- src/helper/java-command.ts
+++ src/helper/java-command.ts
@@ -1,9 +1,9 @@
-import { readdirSync } from 'fs';
+import * as fs from 'fs';
 import * as path from 'path';
 
 import { jrePath } from '../constants';
 import { getExecutable } from './get-executable';
-import { systemJavaExists } from './java-exists';
+import { getSystemJavaHome } from './java-exists';
 
 export async function getJavaCommand(jreInstallPath: string): Promise<string> {
   try {
@@ -12,21 +12,39 @@ export async function getJavaCommand(jreInstallPath: string): Promise<string> {
     // ignore exception
   }
 
-  if (await systemJavaExists()) {
-    return 'java';
+  const javaHome = await getSystemJavaHome();
+  if (javaHome) {
+    return path.join(javaHome, getExecutable());
   }
 
   throw Error('Unable to find locally-installed java or system-wide java');
 }
 
 function getJavaString(jreInstallPath: string): string {
-  const pathOfJreFolder = path.join(path.resolve(jreInstallPath), '../', jrePath);
+  const pathOfJreFolder = path.join(
+    path.resolve(jreInstallPath),
+    '../',
+    jrePath
+  );
 
-  const files = readdirSync(pathOfJreFolder);
-  const file = files.filter((name) => !name.startsWith('._'));
-  if (file.length > 1) {
+  const files = fs.readdirSync(pathOfJreFolder);
+  const file = files
+    .filter((name) => !name.startsWith('._'))
+    .map((name) => path.join(pathOfJreFolder, name, getExecutable()))
+    .filter(hasJavaExe);
+
+  if (file.length == 0) {
     throw Error('JRE installation failed! Please install the package again.');
   }
 
-  return path.join(pathOfJreFolder, file[0], getExecutable());
+  return file[0];
+}
+
+function hasJavaExe(javaExe: string): boolean {
+  try {
+    fs.accessSync(javaExe, fs.constants.X_OK);
+    return true;
+  } catch (e) {
+    return false;
+  }
 }
